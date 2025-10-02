67d7f195ad0ce072cbc716f94502ce1c17c9df9f
Gracia <graciajopson@gmail.com>
2025-10-02 11:54:58 +0530

    Update index.php, login.php and add logo image

--- admin/index.php
+++ admin/index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['id_admin'])) {
 
   
   <title>Placement Portal</title>
-  <link href="../img/logo.png" rel="icon">
+  <link href="../img/christlogo.png" rel="icon">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
